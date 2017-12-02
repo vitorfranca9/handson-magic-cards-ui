dca92a31eec5958322472efa93dd990c9d7082f4
Alexandre Rodrigues <alexandre.rodrigues@softvaro.com.br>
2017-12-02 10:53:18 -0200

    Adicionado list expansion no card-table.js e criado listExpansion.html

--- src/main/resources/public/js/table/cards-table.js
+++ src/main/resources/public/js/table/cards-table.js
@@ -16,4 +16,22 @@ $(document).ready(function() {
             }
         ]
     } );
+    
+    $('.dataTableExpansion').DataTable( {
+        "bFilter":false,
+        "processing": true,
+        "serverSide": true,
+        "ajax": "/cards/data",
+        "columns": [
+            {"data":"expansionId"},
+            {"data":"name"},
+            {"data":"ptBRName"},
+            {
+                "data": null,
+                "render": function (data, type, row, meta) {
+                    return '<a href="/cards/updateExpansion/' + row['expansionId'] + '" class="editor_edit">Edit</a> | <a href="/cards/deleteExpansion/' + row['expansionId'] + '" class="editor_remove">Delete</a>';
+                }
+            }
+        ]
+    } );
 } );
